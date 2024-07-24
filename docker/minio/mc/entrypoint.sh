732ea7f355c1f9f93e2d214e14e387bc58205088
George Pyne <georgepyne@hotmail.co.uk>
2024-07-24 12:49:49 -0700

    titiler config

--- docker/minio/mc/entrypoint.sh
+++ docker/minio/mc/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 
 mc alias set minio http://minio:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 mc mb --ignore-existing minio/stac
-mc mb --ignore-existing minio/index
+mc mb --ignore-existing minio/cog
 
 exec "$@"
